10e3e595a773bb194ba05815eb0b4455d92d929e
lpiem <clement.merlet@etu.univ-lyon1.fr>
2018-10-16 16:14:29 +0200

    [FEAT] Add Card View

---
+++ TPCarnetDeVoyage/app/src/main/java/com/carnetdevoyage/tpcarnetdevoyage/MyAdapter.java
@@ -0,0 +1,10 @@
+package com.carnetdevoyage.tpcarnetdevoyage;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+class MyAdapter extends RecyclerView.Adapter {
+    public MyAdapter(List<MyObject> cities) {
+    }
+}
---
+++ TPCarnetDeVoyage/app/src/main/java/com/carnetdevoyage/tpcarnetdevoyage/MyObject.java
@@ -0,0 +1,20 @@
+package com.carnetdevoyage.tpcarnetdevoyage;
+
+class MyObject {
+    private String text;
+    private String imageUrl;
+
+    public MyObject(String text, String imageUrl) {
+        this.text = text;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getText() {
+        return "test";
+    }
+
+    public Object getImageUrl() {
+    }d
+
+    //getters & setters
+}
